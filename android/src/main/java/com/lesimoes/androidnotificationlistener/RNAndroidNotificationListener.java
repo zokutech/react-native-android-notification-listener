1a4874a1bf34727f606e9b617c96547cab446fbe
Mathew Hall <mhall.js@gmail.com>
2020-12-09 12:51:18 +0000

    Get all notifications on demand (#1)
    * Loop added for collecting all notifications at once. IN PROGRESS
    
    * Export of all notifications working, but only when a notification occurs
    
    * Additional categories added to loop notifications. Category caused crashes
    
    * Tidy up of module. JS file changes
    
    * Date time conversion to iso string. Loop and single extraction functions merged
    
    * Commented code removed
    
    * App name recovered from notification data

--- android/src/main/java/com/lesimoes/androidnotificationlistener/RNAndroidNotificationListener.java
+++ android/src/main/java/com/lesimoes/androidnotificationlistener/RNAndroidNotificationListener.java
@@ -1,47 +1,113 @@
  
 package com.lesimoes.androidnotificationlistener;
 
+import android.app.Notification;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
-import android.app.Notification;
 import android.util.Log;
 
-import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.WritableNativeMap;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
 
 public class RNAndroidNotificationListener extends NotificationListenerService {
     private static final String TAG = "RNAndroidNotificationListener";
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        Notification notification = sbn.getNotification();
+        StatusBarNotification[] nots = getActiveNotifications();
+        WritableMap currentNotification = extractNotificationGoodies(sbn);
+        WritableMap exportedNotificationsMap = new WritableNativeMap();
+        WritableArray not2 = new WritableNativeArray();
+        for (int i = nots.length - 1; i >= 0; i--) {
+            StatusBarNotification noti = nots[i];
+            WritableMap exportMap = extractNotificationGoodies(noti);
+            not2.pushMap(exportMap);
+        }
+        exportedNotificationsMap.putArray("notifications", not2);
+        RNAndroidNotificationListenerModule.sendEvent("allNotifications", exportedNotificationsMap);
+        RNAndroidNotificationListenerModule.sendEvent("notificationReceived", currentNotification);
 
-        if (notification == null || notification.extras == null) return;
-        
-        String app = sbn.getPackageName();
-
-        if (app == null) app = "Unknown";
-
-        CharSequence titleChars = notification.extras.getCharSequence(Notification.EXTRA_TITLE);
-        CharSequence textChars = notification.extras.getCharSequence(Notification.EXTRA_TEXT);
-
-        if (titleChars == null || textChars == null) return;
-        
-        String title = titleChars.toString();
-        String text = textChars.toString();
-
-        if (text == null || text == "" || title == null || title == "") return;
-
-        Log.d(TAG, "Notification received: " + app + " | " + title + " | " + text);
-
-        WritableMap params = Arguments.createMap();
-        params.putString("app", app);
-        params.putString("title", title);
-        params.putString("text", text);
-
-        RNAndroidNotificationListenerModule.sendEvent("notificationReceived", params);
     }
-
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {}
+
+    public WritableMap extractNotificationGoodies(StatusBarNotification noti) {
+        PackageManager pm = getPackageManager();
+        Intent main = new Intent(Intent.ACTION_MAIN, null);
+        main.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> packages = pm.queryIntentActivities(main, 0);
+
+        ArrayList<String> app_name_list = new ArrayList<String>();
+        ArrayList<String> app_package_list = new ArrayList<String>();
+
+        Notification notificationL = noti.getNotification();
+        if (notificationL == null || notificationL.extras == null) return new WritableNativeMap();
+
+        String appL = noti.getPackageName();
+
+        if (appL == null) appL = "Unknown";
+
+        String app_name = null;
+        try {
+            String package_name = appL;
+            app_name = (String) pm.getApplicationLabel(
+                    pm.getApplicationInfo(package_name
+                            , PackageManager.GET_META_DATA));
+            boolean same = false;
+            for (int i = 0; i < app_name_list.size(); i++) {
+                if (package_name.equals(app_package_list.get(i)))
+                    same = true;
+            }
+            if (!same) {
+                app_name_list.add(app_name);
+                app_package_list.add(package_name);
+            }
+            //Log.i("Check", "package = <" + package_name + "> name = <" + app_name + ">");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        CharSequence titleCharsL = notificationL.extras.getCharSequence(Notification.EXTRA_TITLE);
+        CharSequence textCharsL = notificationL.extras.getCharSequence(Notification.EXTRA_TEXT);
+        int numberL = notificationL.number;
+        long dateTimeLongL = notificationL.when;
+        int priorityL = notificationL.priority;
+
+        SimpleDateFormat sdfL;
+        sdfL = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        sdfL.setTimeZone(TimeZone.getTimeZone("UTC"));
+        String isoDateL = sdfL.format(dateTimeLongL);
+
+        if (titleCharsL == null || textCharsL == null) return new WritableNativeMap();
+
+        String titleL = titleCharsL.toString();
+        String textL = textCharsL.toString();
+        String dateTimeL = dateTimeLongL + "";
+
+        if (textL == null || textL == "" || titleL == null || titleL == "")
+            return new WritableNativeMap();
+
+        Log.d(TAG, "Notification in Tray: " + appL + " | " + titleL + " | " + textL);
+        WritableMap paramsL = Arguments.createMap();
+        paramsL.putString("app", appL);
+        paramsL.putString("title", titleL);
+        paramsL.putString("text", textL);
+        paramsL.putInt("number", numberL);
+        paramsL.putString("dateTimeISO", isoDateL);
+        paramsL.putString("dateTimeMS", dateTimeL);
+        paramsL.putInt("priority", priorityL);
+        paramsL.putString("appName", app_name);
+        return paramsL;
+    }
 }
