b6dbca0afbc8b59fb3e829f1ab7081cdfe9805db
Mathew Hall <mhall.js@gmail.com>
2020-12-09 12:46:19 +0000

    App name recovered from notification data

--- android/src/main/java/com/lesimoes/androidnotificationlistener/RNAndroidNotificationListener.java
+++ android/src/main/java/com/lesimoes/androidnotificationlistener/RNAndroidNotificationListener.java
@@ -2,6 +2,9 @@
 package com.lesimoes.androidnotificationlistener;
 
 import android.app.Notification;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
@@ -13,6 +16,8 @@ import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.bridge.WritableNativeMap;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TimeZone;
 
 public class RNAndroidNotificationListener extends NotificationListenerService {
@@ -38,12 +43,40 @@ public class RNAndroidNotificationListener extends NotificationListenerService {
     public void onNotificationRemoved(StatusBarNotification sbn) {}
 
     public WritableMap extractNotificationGoodies(StatusBarNotification noti) {
+        PackageManager pm = getPackageManager();
+        Intent main = new Intent(Intent.ACTION_MAIN, null);
+        main.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> packages = pm.queryIntentActivities(main, 0);
+
+        ArrayList<String> app_name_list = new ArrayList<String>();
+        ArrayList<String> app_package_list = new ArrayList<String>();
+
         Notification notificationL = noti.getNotification();
         if (notificationL == null || notificationL.extras == null) return new WritableNativeMap();
 
         String appL = noti.getPackageName();
 
         if (appL == null) appL = "Unknown";
+
+        String app_name = null;
+        try {
+            String package_name = appL;
+            app_name = (String) pm.getApplicationLabel(
+                    pm.getApplicationInfo(package_name
+                            , PackageManager.GET_META_DATA));
+            boolean same = false;
+            for (int i = 0; i < app_name_list.size(); i++) {
+                if (package_name.equals(app_package_list.get(i)))
+                    same = true;
+            }
+            if (!same) {
+                app_name_list.add(app_name);
+                app_package_list.add(package_name);
+            }
+            //Log.i("Check", "package = <" + package_name + "> name = <" + app_name + ">");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
         CharSequence titleCharsL = notificationL.extras.getCharSequence(Notification.EXTRA_TITLE);
         CharSequence textCharsL = notificationL.extras.getCharSequence(Notification.EXTRA_TEXT);
@@ -62,7 +95,8 @@ public class RNAndroidNotificationListener extends NotificationListenerService {
         String textL = textCharsL.toString();
         String dateTimeL = dateTimeLongL + "";
 
-        if (textL == null || textL == "" || titleL == null || titleL == "") return new WritableNativeMap();
+        if (textL == null || textL == "" || titleL == null || titleL == "")
+            return new WritableNativeMap();
 
         Log.d(TAG, "Notification in Tray: " + appL + " | " + titleL + " | " + textL);
         WritableMap paramsL = Arguments.createMap();
@@ -73,6 +107,7 @@ public class RNAndroidNotificationListener extends NotificationListenerService {
         paramsL.putString("dateTimeISO", isoDateL);
         paramsL.putString("dateTimeMS", dateTimeL);
         paramsL.putInt("priority", priorityL);
+        paramsL.putString("appName", app_name);
         return paramsL;
     }
 }
